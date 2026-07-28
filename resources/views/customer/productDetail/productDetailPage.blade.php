0899919d2caf78c35be08678f59b1b8d5b8e518e
SuMyatPhyuSin25 <sumyatphyu25@gmail.com>
2026-07-28 10:44:36 +0630

    fix:product detail page

--- resources/views/customer/productDetail/productDetailPage.blade.php
+++ resources/views/customer/productDetail/productDetailPage.blade.php
@@ -493,10 +493,10 @@
                                                 <div class="p-4 border border-secondary border-top-0 rounded-bottom">
                                                     <h4>{{ $productList->name }}</h4>
                                                    
-                                                  <p>{{Str::words($productList->description, 10, '...')}}</p>
+                                                  {{-- <p>{{Str::words($productList->description, 10, '...')}}</p> --}}
                                                     <div class="d-flex justify-content-between flex-lg-wrap">
                                                         <p class="text-dark fs-5 fw-bold mb-0">{{$productList->price}} MMK /kg</p>
-                                                        <a href="#" class="btn border border-secondary rounded-pill px-3 text-primary"><i class="fa fa-shopping-bag me-2 text-primary"></i> Add to cart</a>
+                                                   
                                                     </div>
                                                 </div>
                                             </div>
